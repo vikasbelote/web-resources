673a1e3b4a43bb994d0c9150e523d875cdb803a6
tejalsalunkhe <tejalsalunkhe70@gmail.com>
2018-07-08 19:58:50 +0530

    added Changes related to dashboard for loading values from db

--- web-resources/assets/js/suntan/controller/dashboard-controller.js
+++ web-resources/assets/js/suntan/controller/dashboard-controller.js
@@ -3,7 +3,7 @@ suntanApp.controller('DashboardController', function($scope, $http, $rootScope) 
         $http.get("/user")
         .then(function(response) {
             $rootScope.userData = response.data;
-        });   
+        });
     }
     $scope.init();
 
@@ -11,4 +11,27 @@ suntanApp.controller('DashboardController', function($scope, $http, $rootScope) 
     $scope.editProfile = function() {
         alert(1);
     }
+});
+
+suntanApp.controller('DashboardContentController', function($scope, $http, $rootScope) {
+
+var res = $http.get('/investment-service/getDashboardContent?personId=' + $rootScope.userData.personModel.personId);
+        res.success(function(data, status, headers, config) {
+            $scope.investmentList = data;
+            var totalInvestment=0;
+            var totalProfit=0;
+            for(i=0;i<$scope.investmentList.length;i++){
+            totalInvestment += $scope.investmentList[i].amount;
+            totalProfit += $scope.investmentList[i].outcome;
+            }
+            $scope.totalInvestment = totalInvestment;
+            $scope.totalProfit = totalProfit;
+            $scope.percentage = (Math.round(totalProfit * 100) / totalInvestment).toFixed(2);
+
+		});
+		res.error(function(data, status, headers, config) {
+            NotificationService.danger();
+		});
+
+
 });
