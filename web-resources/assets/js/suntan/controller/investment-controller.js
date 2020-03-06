96410733a823f83b0dc0dbc03fde1643ba08d212
Siddharth Tiwari <siddharth.tiwari@davita.com>
2020-03-06 16:01:47 +0530

    suntan capital changes done

--- web-resources/assets/js/suntan/controller/investment-controller.js
+++ web-resources/assets/js/suntan/controller/investment-controller.js
@@ -26,13 +26,13 @@ suntanApp.controller('AddInvestmentController', function($scope, $http, $rootSco
             NotificationService.success();
 		});
 		res.error(function(data, status, headers, config) {
-            alert( "failure message: " + JSON.stringify({data: data}));
+            //alert( "failure message: " + JSON.stringify({data: data}));
             NotificationService.danger();
 		});
     }
 });
 
-suntanApp.controller('MyInvestmentController', function($scope, $http, $rootScope) {
+suntanApp.controller('MyInvestmentController', function($scope, $http, $rootScope, NotificationService) {
     $scope.init = function() {
         var personId =  parseInt($rootScope.userData.personModel.personId);
         var res = $http.get('/investment-service/my-investments?personId=' + personId);
@@ -41,9 +41,86 @@ suntanApp.controller('MyInvestmentController', function($scope, $http, $rootScop
             $scope.investments = data;
 		});
 		res.error(function(data, status, headers, config) {
+		    //alert('not done');
             NotificationService.danger();
         });
         //TableManageDefault.init();
     }
     $scope.init();
 });
+
+suntanApp.controller('ProfileController', function($scope, $http, $rootScope, NotificationService) {
+    $scope.init = function() {
+        var personId =  parseInt($rootScope.userData.personModel.personId);
+        var res = $http.get('/investment-service/my-profile?personId=' + personId);
+		res.success(function(data, status, headers, config) {
+            //console.log(data)
+            $scope.personDomain = data;
+		});
+		res.error(function(data, status, headers, config) {
+		    //alert('not done');
+            NotificationService.danger();
+        });
+        //TableManageDefault.init();
+    }
+    $scope.init();
+
+    $scope.saveProfile = function(personDomain) {
+            console.log("Profile controller");
+            console.log(personDomain)
+            console.log(JSON.stringify(personDomain));
+
+            //investment.personId = $rootScope.userData.personModel.personId;
+            //console.log(investment);
+
+            var res = $http.post('/investment-service/edit-profile', personDomain);
+    		res.success(function(data, status, headers, config) {
+                $scope.message = data;
+                //console.log(data)
+                NotificationService.success();
+    		});
+    		res.error(function(data, status, headers, config) {
+                //alert( "failure message: " + JSON.stringify({data: data}));
+                NotificationService.danger();
+    		});
+        }
+});
+
+suntanApp.filter('blankNULL', function () {
+  return function (text) {
+
+    if(text=="NULL")
+        return "";
+
+    return text;
+  };
+});
+
+/*$scope.logout = function() {
+    logout($scope.userData);
+}
+function logout(params) {
+    var req = {
+        method: 'DELETE',
+        url: "oauth/token"
+    }
+    $http(req).then(
+        function(data){
+            $cookies.remove("access_token");
+            window.location.href="login";
+        },function(){
+            console.log("error");
+        }
+    );
+}*/
+
+/*
+function logout() {
+    this.http.post('logout', {}).finally(() => {
+        self.authenticated = false;
+        this.http.post('http://localhost:8091/logout', {}, {withCredentials:true})
+            .subscribe(() => {
+                console.log('Logged out');
+        });
+    }).subscribe();
+};*/
--- web-resources/assets/js/suntan/main.js
+++ web-resources/assets/js/suntan/main.js
@@ -2,22 +2,22 @@ var suntanApp = angular.module("suntanApp", ['ngRoute']);
 
 suntanApp.config(function($routeProvider) {
     $routeProvider
-        .when('/', {
-            templateUrl: './dashboard/dashboard-content.html',
-            controller: 'DashboardController'
+		.when('/my-investment', {
+            templateUrl: './investment/my.html',
+            controller: 'MyInvestmentController'
         })
 		.when('/add-investment/:investmentId?', {
 			templateUrl: './investment/add.html',
 			controller: 'AddInvestmentController'
 		})
-		.when('/my-investment', {
-			templateUrl: '/investment/my.html',
-			controller: 'MyInvestmentController'
-		})
 		.when('/edit-profile', {
-			templateUrl: '/profile.html',
-			controller: 'DashboardController'
+			templateUrl: './profile.html',
+			controller: 'ProfileController'
 		})
+		.when('/', {
+            templateUrl: './dashboard/dashboard-content.html',
+            controller: 'DashboardController'
+        })
 		.otherwise({
 			redirectTo: '/'
 		});
